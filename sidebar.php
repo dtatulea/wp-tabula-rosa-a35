56bffccba70816b9dfaa4de571a2dc692e3d68d2
Dragos Tatulea <dragos.tatulea@intel.com>
2012-08-18 17:50:11 +0300

    initial commit of tabula-rosa from atelier35.eu

---
+++ sidebar.php
@@ -0,0 +1,27 @@
+	<div id="left-col">
+		<?php custom_nav_menu(); ?>
+
+
+		<div id="sidebar">
+
+
+			<?php 	/* Widgetized sidebar, if you have the plugin installed. */
+					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('main_sidebar') ) : ?>
+			
+			<div class="widget static-widget">
+				<h3 class="widgettitle">Archives</h3>
+				<ul>
+					<?php wp_get_archives('type=monthly'); ?>
+				</ul>
+			</div>
+			
+			<div class="widget static-widget">
+				<h3 class="widgettitle">Categories</h3>
+				<ul>
+					<?php wp_list_categories('show_count=0&title_li=&hierarchical=0'); ?>
+				</ul>
+			</div>
+		<?php endif; ?>
+		</div>
+	</div>
+
