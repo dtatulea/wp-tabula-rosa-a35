8e5026409ce58b5dbe7cb0da1af9e70a1d6034ce
Dragos Tatulea <dragos.tatulea@gmail.com>
2012-08-18 17:50:11 +0300

    initial commit of tabula-rosa from atelier35.eu

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div>
+
+	<div id="footer">
+<div id="uap">
+<a href="http://uap.ro" target="_blank"><img src="/uap.gif"></a>
+Spatiul Atelier 35 este pus la dispozitie de Uniunea Artistilor Plastici
+</div>
+		<p>&copy; 2010 <?php bloginfo('name'); ?> | Powered by <a href="http://wordpress.org/">WordPress</a> | Theme: <a href="http://leonewball.com/portfolio/tabula-rosa/">tabula rosa</a> | <a href="<?php bloginfo('rss2_url'); ?>">RSS Feed</a></p>
+	</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
