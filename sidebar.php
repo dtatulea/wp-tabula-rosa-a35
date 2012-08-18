a5d083a38b3267c10b6a4f1ba807a3216016844a
Dragos Tatulea <dragos.tatulea@intel.com>
2012-08-18 17:52:51 +0300

    add menu-like class for widget titles that want to immitate menus

--- sidebar.php
+++ sidebar.php
@@ -8,14 +8,14 @@
 			<?php 	/* Widgetized sidebar, if you have the plugin installed. */
 					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('main_sidebar') ) : ?>
 			
-			<div class="widget static-widget">
+			<div class="widget static-widget menu-like">
 				<h3 class="widgettitle">Archives</h3>
 				<ul>
 					<?php wp_get_archives('type=monthly'); ?>
 				</ul>
 			</div>
 			
-			<div class="widget static-widget">
+			<div class="widget static-widget menu-like">
 				<h3 class="widgettitle">Categories</h3>
 				<ul>
 					<?php wp_list_categories('show_count=0&title_li=&hierarchical=0'); ?>
